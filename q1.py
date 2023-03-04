45a2cfe241074e1cfd3d6710bc2a54613c6e5d7d
tenochg <yomoozuki@gmail.com>
2023-03-03 22:22:32 -0800

    LPC Q-1

--- q1.py
+++ q1.py
@@ -3,10 +3,21 @@
 # Make your Code
 # ******************************
 count = 0
+flag = False
+
 for i in range(10):
-  x = int(input('Enter a number: '))
-  if x % 2 == 0:
-    count = count + 1
+    x = int(input("Enter a number: "))
+    if x % 2 == 0:
+        if not flag:
+            count += 1
+            flag = True
+    else:
+        flag = False     
+    
+    
+    
+    
+
 print(count)
 
 
