bcea6dba67f8f89fd72f9fb83f784b3be826622a
tenochg <yomoozuki@gmail.com>
2023-03-04 00:44:36 -0800

    question 4-1

--- q1.py
+++ q1.py
@@ -2,27 +2,23 @@
 # ******************************
 # Make your Code
 # ******************************
-count = []
 flag = 0
-
+count = 0
 for i in range(10):
-    num = int(input("Enter a number: "))
-    count.append(num)
-
-totalc = 0
-
-for i in count:
-    if i % 2 == 0:
+    num = int(input('Enter a number: '))
+    if num % 2 == 0:
         flag = flag + 1
-    else: 
-        if flag > 0:
-            totalc = totalc + 1
+    else:
+        if flag > 1:
+            count =  count + 1
             flag = 0
-if flag > 0:
-    totalc = totalc + 1
+        
+    
+print(count)
+
 
     
-print(totalc)
+
         
 
 
